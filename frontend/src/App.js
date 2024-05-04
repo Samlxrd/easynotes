2461dc49b72c3c848651290ae63807cf1613a399
mychaelmatos <mychaelantoniomatos@gmail.com>
2024-05-04 18:09:08 -0300

    creact components header/footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/header/header';
+import Footer from './components/footer/footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <h1>Login</h1>
+      <form action="teste.jsx" method='GET'>
+        <label htmlFor="name">Name:</label>
+        <input type="text" name="name" />
+        <label htmlFor="password">Password:</label>
+        <input type="password" name="password" />
+        <input type="submit" value="Submit" />
+      </form>
+      <Footer/>
     </div>
   );
 }
---
+++ frontend/src/components/footer/footer.jsx
@@ -0,0 +1,11 @@
+import './footer.css';
+
+function Footer() {
+  return (
+    <footer>
+      <h1>Footer</h1>
+    </footer>
+  );
+}
+
+export default Footer;
