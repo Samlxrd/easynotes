926a88611cd24998d366aeb98b70b636015cc618
Samlxrd <samlord.dzn@gmail.com>
2024-08-12 11:17:23 -0300

    login

---
+++ frontend/src/components/loginForm/loginForm.jsx
@@ -0,0 +1,24 @@
+import './loginForm.css';
+
+function LoginForm() {
+    return (
+        <div className="form">
+            <h3>Entrar</h3>
+            <form action="">
+                <div className="form-group">
+                    <label for="username">Usuário</label>
+                    <input type="text" id="username" name="username" />
+                </div>
+
+                <div className="form-group">
+                    <label for="password">Senha</label>
+                    <input type="password" id="password" name="password" />
+                </div>
+
+                <button type="submit">Entrar</button>
+        </form>
+    </div>
+    );
+  }
+  
+  export default LoginForm;
